c0557525bbb9996b52cb247bd215f77a72e2bae7
Chayene Mae Banta <53049505+chayenemae@users.noreply.github.com>
2021-08-11 09:05:57 -0400

    Updated version

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # This is a repository that contains stuff that CSE team will find handy
 
 # To create the standard AWS configuration file
-1. First, make a copy of the config from this repo
-2. sed -i ‘s/search_string/replace_string/’ filename
+1. Install aws-vault
+2. Run aws-vault add default
+3. From the aws-credentials folder, run create-config.sh then enter your AWS shortname
--- aws-credentials/create-config.sh
+++ aws-credentials/create-config.sh
@@ -1,2 +1,12 @@
-cp config /Users/cbanta/.aws/auto-config
-sed 's/username/user5/' /Users/cbanta/.aws/auto-config
+#Get the users shortname
+echo “Enter your username ie. cbanta: ” 
+read shortname
+
+#Make a backup of the insisting config file
+cp /Users/cbanta/.aws/config /Users/cbanta/.aws/config.backup
+
+#Copy the local template into the users aws config directory
+cp config /Users/cbanta/.aws/config
+
+#Replace "username" with shortname input
+sed -i -e "s/username/$shortname/g" "/Users/cbanta/.aws/config"
